aae56b76fd94c2ccc628a370615117f4f3622cac
Andrea Venier <andrea.venier.pwc@gmail.com>
2022-03-02 18:54:51 +0100

    Added Async Await example

--- SalesforceAccademy2022/force-app/main/default/lwc/asyncAwait/asyncAwait.js
+++ SalesforceAccademy2022/force-app/main/default/lwc/asyncAwait/asyncAwait.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
+import getAllAccount from '@salesforce/apex/AccountSelector.getAll';
 
-export default class AsyncAwait extends LightningElement {}
+export default class AsyncAwait extends LightningElement {
+
+    async connectedCallback(){
+        try {
+            let result = await getAllAccount();
+            console.log(result);
+        } catch (error) {
+            console.error(error);un 
+        }
+    }
+
+    /*connectedCallback(){
+        getAllAccount()
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }*/
+}
